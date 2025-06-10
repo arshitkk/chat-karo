eff6a3248de38ac49404ae3f51762ed2c2bebe46
Arshit Kumar <arshitkumar222@gmail.com>
2025-06-11 04:45:22 +0530

    Fixing deployment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
 export const THEMES = [
   "light",
   "dark",
