2b05d86adce0355784c5201361d56840c71d6a93
Arshit Kumar <arshitkumar222@gmail.com>
2025-06-11 11:29:45 +0530

    Fixed sidebar padding

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -38,7 +38,7 @@ const Sidebar = () => {
   const filteredUsers = ShowOnlineOnly ? onlineFilteredUsers : users;
   return (
     <aside className="h-full  lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
-      <div className="border-b border-base-300 w-full p-5">
+      <div className="border-b border-base-300 w-full p-1">
         <div className="flex items-center gap-2">
           <Users className="size-6 ml-6" />
           <span className="font-medium hidden lg:block ">Contacts</span>
